0779fde92f2207326dd3f2b81661928d115e1ea6
jclee5419 <jclee5419@gmail.com>
2025-04-22 18:51:08 +0900

    ✨ 붕어빵 주문 기능 구현
    메뉴 뒤로가기 생성, 주문받은 붕어빵 판매 기능, 판매된 붕어빵 개수 출력 기능, 붕어빵 맛 오입력 검출 기능

--- fishbread_shop.py
+++ fishbread_shop.py
@@ -3,8 +3,41 @@
 
 i = 0
 
+stock = { # key값을 이용해서 value  딕셔너리를 써야하는 상황은 어떤 스토리 기반으로 데이터가 구성되었을 떄
+    "팥붕어빵": 10,
+    "슈크림붕어빵": 7,
+    "초코붕어빵": 4,
+}
+
+sales = {
+    "팥붕어빵": 0,
+    "슈크림붕어빵": 0,
+    "초코붕어빵": 0,
+}
+
+def order_bread():
+    while True:
+        bread_type = input("붕어빵을 선택해주세요(팥붕어빵, 슈크림붕어빵, 초코붕어빵) 만약 뒤로가길 원한시면 뒤로가기를 입력해주세요")
+        if bread_type == "뒤로가기":
+            print("뒤로가기")
+            break
+        if bread_type in stock:
+            bread_count = int(input("주문할 개수를 입력하세요: "))
+            if stock[bread_type] > bread_count: # 창고에 있는 빵의 개수가 니가 주문한 개수보다 많은지 확인해 볼게(조건)
+                stock[bread_type] -= bread_count
+                sales[bread_type] =+ bread_count
+                print(f'{bread_type} {bread_count}개가 판매 되었습니다.')
+            else:
+                print(f'재고가 부족합니다. 현재{stock[bread_type]}개만 주문 가능합니다.') # 미안해 지금 주문 가능한 빵의 개수는 00개야
+        else:
+            print("잘못된 시도입니다 다시 시도하십시오")
+
+
+
+
+
 while True:
-    mode = input("원하는 모드를 선택하세요(주문, 관리자, 종료):")
+    mode = input("원하는 모드를 선택하세요(주문, 관리자, 종료):") # 주문
     if mode == "종료":
         print("시스템이 종료되었습니다.")
         break
